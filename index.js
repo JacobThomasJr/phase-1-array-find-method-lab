091a13d41a467b3b9f8cc705c6d4ac0e66c37aa6
jacobaranza <jacobthomasjunior@gmail.com>
2023-04-22 15:22:47 -0500

    array find method lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// code your solution here
+function superbowlWin(superbowlData) {
+    // Use the find() function to search for an object with result === "W"
+    const win = superbowlData.find(superbowl => superbowl.result === "W");
+    
+    // If a win is found, return the year property, otherwise return undefined
+    return win ? win.year : undefined;
+  }
+  
